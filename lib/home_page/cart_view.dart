14cedb4623eb6f1c69f0e712dc8ff618ae219562
jahir_abs <jahir@webmaddy.com>
2022-06-03 18:24:00 +0530

    3rd june commitnew update

--- lib/home_page/cart_view.dart
+++ lib/home_page/cart_view.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:indian_coin_shop/app_constant/style_constant.dart';
 import 'package:indian_coin_shop/home_page/widgets/cart_item.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
-class CartView extends StatelessWidget {
+class CartView extends StatefulWidget {
   const CartView({Key? key}) : super(key: key);
+
+  @override
+  State<CartView> createState() => _CartViewState();
+}
+
+class _CartViewState extends State<CartView> {
+  late Razorpay _razorpay;
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "SUCCESS: ${response.paymentId}",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 4,
+        backgroundColor: Colors.green,
+        textColor: StyleConstants.colorWhite,
+        fontSize: 16.0
+    );
+
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    //Fluttertoast.showToast(msg: "ERROR: " + response.code.toString() + " - " + response.message, timeInSecForIos: 4);
+    Fluttertoast.showToast(
+        msg: "ERROR: ${response.code.toString()}",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 4,
+        backgroundColor: Colors.green,
+        textColor: StyleConstants.colorWhite,
+        fontSize: 16.0
+    );
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    //Fluttertoast.showToast(msg: "EXTERNAL_WALLET: " + response.walletName, timeInSecForIos: 4);
+    Fluttertoast.showToast(
+        msg: "EXTERNAL_WALLET: ${ response.walletName}",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 4,
+        backgroundColor: Colors.green,
+        textColor: StyleConstants.colorWhite,
+        fontSize: 16.0
+    );
+  }
+
+  void openCheckout() async {
+    var options = {
+      'key': 'rzp_test_NNbwJ9tmM0fbxj',
+      'amount': 28200,
+      'name': 'Shaiq',
+      'description': 'Payment',
+      'prefill': {'contact': '8888888888', 'email': 'test@razorpay.com'},
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
